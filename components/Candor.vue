cc9cf59b83d8288f0d40a7c4eb1bca07fd78400f
Timon Loeffen <timon.loeffen@aixigo.com>
2021-08-17 19:23:37 +0200

    mulitply!

--- components/Candor.vue
+++ components/Candor.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="bg-bluegray text-white max-w-screen-xl mx-auto px-8 py-16 flex justify-evenly">
-    <div class="max-w-xl ml-16">
+  <section class="bg-bluegray text-white max-w-screen-xl mx-auto flex">
+    <div class="max-w-lg ml-16 mt-32 pr-32">
       <h1 class="flex flex-col">
         <span class="font-roofline text-sunset uppercase text-lg">Meine Werte</span>
         <span class="font-screaming text-6xl">Ganz ehrlich</span>
@@ -9,8 +9,9 @@
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam commodi corrupti nulla quisquam quas vero, possimus natus praesentium, harum consequuntur aspernatur voluptatem neque ex optio itaque ipsam iste ut rem?</p>
     </div>
     <figure class="relative">
-      <img src="~/assets/images/Gruppe_310.jpg" alt="Happy guests">
-      <figcaption class="max-w-xl filter drop-shadow font-wavy ml-8 mb-16 text-white text-8xl absolute bottom-0 left-0">
+      <img class="object-contain" src="~/assets/images/Gruppe_310.jpg" alt="Happy guests">
+      <div class="absolute top-0 bottom-0 right-1/2 left-0 bg-gradient-to-r from-black opacity-50"></div>
+      <figcaption class="max-w-md filter drop-shadow font-wavy text-white text-6xl text-right absolute top-8 -left-96">
         Embrace the flaws and the gems
       </figcaption>
     </figure>
