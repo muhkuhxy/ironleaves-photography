a2284c70809f1ca176dab67ac605fde966135616
Timon Loeffen <timon.loeffen@aixigo.com>
2021-08-17 22:54:26 +0200

    fix responsive hero image

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <figure class="relative shadow-lg">
-      <img class="object-cover w-screen m-h-9/10"
+      <img class="object-cover w-full max-h-9/10"
         src="~/assets/images/Pfad-1.jpg"
         alt="Glückliches Ehepaar">
       <figcaption class="sm:max-w-md md:max-w-2xl filter drop-shadow font-wavy text-white absolute bottom-0 left-0 ml-9 mb-6 md:ml-24 md:mb-12">
