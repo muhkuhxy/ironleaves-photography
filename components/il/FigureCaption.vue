485b3e9f96e848bb85aae3c7445407d0fc46f4d4
Timon Loeffen <timon.loeffen@aixigo.com>
2021-08-19 23:59:17 +0200

    responsive font sizes

--- components/il/FigureCaption.vue
+++ components/il/FigureCaption.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <div class="absolute top-0 bottom-0 right-1/2 left-0 bg-gradient-to-r from-black opacity-50"></div> -->
-  <figcaption class="filter drop-shadow font-wavy absolute bottom-0 left-0 ml-8 mb-6">
+  <figcaption class="caption filter drop-shadow absolute bottom-0 left-0 ml-8 mb-6">
     <slot/>
   </figcaption>
 </template>
--- components/section/Header.vue
+++ components/section/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 class="">
-    <span class="block text-sunset font-roofline uppercase"><slot name="roofline"/></span>
-    <span class="font-screaming font-bold text-3xl sm:text-4xl"><slot name="default"/></span>
+    <span class="roofline block text-sunset uppercase"><slot name="roofline"/></span>
+    <span class="headline font-bold"><slot name="default"/></span>
   </h1>
 </template>
