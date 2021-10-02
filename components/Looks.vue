439f7b124674a8fd1f312ab88a4e0e93d22001e6
Timon Loeffen <timon.loeffen@aixigo.com>
2021-10-02 22:20:40 +0200

    looks desktop

--- components/Looks.vue
+++ components/Looks.vue
@@ -1,33 +1,36 @@
 <template>
-  <SectionParent class="bg-emerald text-white">
-    <IlFigure>
-      <img class="object-cover w-full"
+  <SectionParent class="bg-emerald text-white lg:relative">
+    <IlFigure class="lg:absolute lg:w-50% 2xl:w-40% lg:inset-y-0 lg:left-0">
+      <img class="object-cover w-full h-full"
         src="~/assets/images/Sandra-und-Jonas-Standesamt-Shooting-6105.jpg"
         alt="Happy couple">
       <IlGradient :half-height="true" />
     </IlFigure>
-    <SvgThreeStraws class="text-white fill-current float-right relative right-0 mr-1 -mt-32 w-1/4" />
+    <SvgThreeStraws class="text-white fill-current float-right lg:float-none relative lg:absolute right-0 lg:bottom-0 lg:left-0 -mt-32 lg:mt-0 mr-1 lg:-mb-4 w-1/4 lg:w-auto lg:h-50% lg:ml-35%" />
+    <SvgHeroLine2 class="hidden 2xl:block absolute text-sunset opacity-75 stroke-current stroke-2 fill-none h-overflow -mt-2.5% ml-33%" />
     <SectionContent>
-      <SectionHeader>
-        <template #roofline>Bildlooks</template>
-        Die Welt ist bunt &ndash; und ihr seid es auch!
-      </SectionHeader>
-      <IlSpacer />
-      <p class="mb-4 max-w-prose">
-        Immerhin ist es euer besonderer Tag! Die Fotos
-        sollten also euch widerspiegeln. Ihr bestimmt
-        also den Bildlook eurer Bilder in
-        Zusammenarbeit mit mir. Dabei könnt ihr unter
-        unterschiedlichen Atmosphären den Look
-        auswählen, der am besten zu euch passt!
-      </p>
-      <p class="max-w-prose">
-        Natürlich stehe ich euch dabei tatkräftig
-        beratend zur Seite und erkläre euch, worauf es
-        ankommt.
-        Seid ihr schon gespannt auf Eure Hochzeit? Ich halte eure unvergesslichen Momente, die zahlreichen Emotionen, eure ganz besondere
-        intime Bindung zueinander und diesen wichtigen Tag in eurem Leben für euch mit viel Leidenschaft fest!
-      </p>
+      <div class="lg:py-24 lg:ml-55% 2xl:ml-45%">
+        <SectionHeader>
+          <template #roofline>Bildlooks</template>
+          Die Welt ist bunt &ndash; und ihr seid es auch!
+        </SectionHeader>
+        <IlSpacer class="md:mt-4 xl:mt-8" />
+        <p class="max-w-prose">
+          Immerhin ist es euer besonderer Tag! Die Fotos
+          sollten also euch widerspiegeln. Ihr bestimmt
+          also den Bildlook eurer Bilder in
+          Zusammenarbeit mit mir. Dabei könnt ihr unter
+          unterschiedlichen Atmosphären den Look
+          auswählen, der am besten zu euch passt!
+        </p>
+        <p class="max-w-prose mt-4">
+          Natürlich stehe ich euch dabei tatkräftig
+          beratend zur Seite und erkläre euch, worauf es
+          ankommt.
+          Seid ihr schon gespannt auf Eure Hochzeit? Ich halte eure unvergesslichen Momente, die zahlreichen Emotionen, eure ganz besondere
+          intime Bindung zueinander und diesen wichtigen Tag in eurem Leben für euch mit viel Leidenschaft fest!
+        </p>
+      </div>
     </SectionContent>
   </SectionParent>
 </template>
