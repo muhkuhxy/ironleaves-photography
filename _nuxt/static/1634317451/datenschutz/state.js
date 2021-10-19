70b474de938e013b218176872d2a98559914f18f
muhkuhxy <muhkuhxy@users.noreply.github.com>
2021-10-19 20:32:52 +0000

    deploy: a0939dab9c5e63ee889efc684a70481f9773aead

--- _nuxt/static/1634317451/datenschutz/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fironleaves-photography\u002F_nuxt\u002Fstatic\u002F1634317451",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdatenschutz",config:{_app:{basePath:"\u002Fironleaves-photography\u002F",assetsPath:"\u002Fironleaves-photography\u002F_nuxt\u002F",cdnURL:a}}}}(null));
