42a9efddcb1b37b6f6a11559d52a738ab45fadb9
Timon Loeffen <timon.loeffen@aixigo.com>
2021-11-03 13:31:40 +0100

    papeterie desktop

--- components/Papeterie.vue
+++ components/Papeterie.vue
@@ -1,27 +1,33 @@
 <template>
   <SectionParent>
-    <div class="bg-bluegray text-white overflow-y-hidden">
-      <IlFigure>
-        <img class="object-cover w-full"
+    <div class="bg-bluegray text-white overflow-y-hidden relative">
+      <IlFigure class="lg:absolute lg:w-50% 2xl:w-40% lg:inset-y-0 lg:right-0">
+        <img class="object-cover w-full h-full"
           src="~/assets/images/Valerie-und-Max-712@2x.jpg"
           alt="Happy guests">
         <IlGradient direction="top-right" />
-        <IlFigureCaption>
+        <IlFigureCaption class="lg:text-right lg:top-0 lg:pt-12 lg:px-0 transform lg:-translate-x-40% lg:w-max">
           Handmade<br>with love
         </IlFigureCaption>
       </IlFigure>
-      <SvgFlowerWhite class="shape-poly w-1/4 float-right transform -translate-x-8 -translate-y-24 text-white fill-current"/>
+      <SvgFlowerWhite class="shape-poly w-1/4 lg:w-15% 2xl:w-13% float-right lg:float-none lg:absolute transform -translate-x-8 -translate-y-24 lg:transform-none lg:right-0 lg:bottom-0 lg:mr-40% 2xl:mr-30% text-white fill-current "/>
+      <div class="hidden xl:block absolute z-10 inset-0 overflow-hidden">
+        <SvgHeroLine1 class="absolute text-white opacity-25 stroke-current stroke-2 fill-none h-overflow -mt-2.5% ml-37% 2xl:ml-50%" />
+        <SvgHeroLine2 class="absolute text-sunset opacity-50 stroke-current stroke-2 fill-none h-overflow -mt-2.5% ml-48% xl:ml-49% 2xl:ml-55%" />
+      </div>
       <SectionContent>
-        <SectionHeader>
-          <template #roofline>Papeterie</template>
-          So individuell wie ihr!
-        </SectionHeader>
-        <IlSpacer />
-        <p>Egal was ihr braucht – ich helfe euch, ein Design zu entwickeln, was zu
-  eurer Hochzeit passt. Wir finden gemeinsam heraus, wie euer Stil ist und
-  dann können wir loslegen.</p>
-        <p class="mt-4">Von den Save-the-Date-Karten bis hin zum Dankesgeschenk machen wir
-  alles in eurem Stil.</p>
+        <div class="lg:py-32 lg:mr-55% 2xl:mr-45%">
+          <SectionHeader>
+            <template #roofline>Papeterie</template>
+            So individuell wie ihr!
+          </SectionHeader>
+          <IlSpacer class="md:mt-4 xl:mt-8" />
+          <p class="max-w-prose">Egal was ihr braucht – ich helfe euch, ein Design zu entwickeln, was zu
+    eurer Hochzeit passt. Wir finden gemeinsam heraus, wie euer Stil ist und
+    dann können wir loslegen.</p>
+          <p class="max-w-prose mt-4">Von den Save-the-Date-Karten bis hin zum Dankesgeschenk machen wir
+    alles in eurem Stil.</p>
+        </div>
       </SectionContent>
     </div>
     <IlSlider class="pb-8 h-50vh" :slides="slides" />
