6e0500086f006b66e1a017a6feb0a1c549cdd750
Timon Loeffen <timon.loeffen@aixigo.com>
2021-11-10 16:15:55 +0100

    fix: looks svg line extends beyond section bottom

--- components/Looks.vue
+++ components/Looks.vue
@@ -1,5 +1,5 @@
 <template>
-  <SectionParent class="bg-emerald text-white lg:relative">
+  <SectionParent class="bg-emerald text-white lg:relative overflow-y-hidden">
     <IlFigure class="lg:absolute lg:w-50% 2xl:w-40% lg:inset-y-0 lg:left-0">
       <img
        class="object-cover w-full h-full"
@@ -7,8 +7,8 @@
         alt="Happy couple">
       <IlGradient :half-height="true" />
     </IlFigure>
-    <SvgThreeStraws class="text-white fill-current float-right lg:float-none relative lg:absolute right-0 lg:bottom-0 lg:left-0 -mt-32 lg:mt-0 mr-1 lg:-mb-4 w-1/4 lg:w-auto lg:h-50% lg:ml-35%" />
     <SvgHeroLine2 class="hidden 2xl:block absolute text-sunset opacity-75 stroke-current stroke-2 fill-none h-overflow -mt-2.5% ml-37%" />
+    <SvgThreeStraws class="text-white fill-current float-right lg:float-none relative lg:absolute right-0 lg:bottom-0 lg:left-0 -mt-32 lg:mt-0 mr-1 lg:-mb-4 w-1/4 lg:w-auto lg:h-50% lg:ml-35%" />
     <SectionContent>
       <div class="lg:py-24 lg:ml-55% 2xl:ml-45%">
         <SectionHeader>
