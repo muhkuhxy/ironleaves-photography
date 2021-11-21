3e28789354d3d6253968d482440e213743aa3c23
muhkuhxy <muhkuhxy@users.noreply.github.com>
2021-11-21 18:53:03 +0000

    deploy: 8c58624cd010c93ecb25aa3c103385e6851c4d45

--- _nuxt/static/1637520733/AGBs/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fironleaves-photography\u002F_nuxt\u002Fstatic\u002F1637520733",layout:"default",error:a,state:{animationReady:false},serverRendered:true,routePath:"\u002FAGBs",config:{_app:{basePath:"\u002Fironleaves-photography\u002F",assetsPath:"\u002Fironleaves-photography\u002F_nuxt\u002F",cdnURL:a}}}}(null));
