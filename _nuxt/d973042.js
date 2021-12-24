af73968438b168879005cf4e5c1b7cb3d9f393c8
muhkuhxy <muhkuhxy@users.noreply.github.com>
2021-12-24 13:19:57 +0000

    deploy: c7ea013b18f11f102a38a19fee87bf5060efe8a7

--- _nuxt/d973042.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{329:function(e,t,n){"use strict";n.r(t);var l=n(8),component=Object(l.a)({},(function(){var e=this,t=e.$createElement;return(e._self._c||t)("figure",{staticClass:"relative overflow-hidden"},[e._t("default")],2)}),[],!1,null,null,null);t.default=component.exports}}]);
