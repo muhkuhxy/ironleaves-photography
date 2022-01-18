d6f0dbd2e7ea928fe98f13a096b17109161f66ff
muhkuhxy <muhkuhxy@users.noreply.github.com>
2022-01-18 18:36:11 +0000

    deploy: b347ddf7a49b0db4423182951b4d36b1d4e43404

--- _nuxt/static/1642529828/stories/infos/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fironleaves-photography\u002F_nuxt\u002Fstatic\u002F1642529828",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstories\u002Finfos",config:{_app:{basePath:"\u002Fironleaves-photography\u002F",assetsPath:"\u002Fironleaves-photography\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"28466c2c"}}}}(null));
