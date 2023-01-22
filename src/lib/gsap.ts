c9bf48351fbc77501997fdaa570a4224f8a0ee4e
Timon Loeffen <timon.loeffen@sigmatechnology.com>
2023-01-22 15:21:28 +0100

    convert candor section

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -3,28 +3,28 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger)
 
-export function slideUp(options: { delay?: string, y?: string }, el: HTMLElement) {
+export function slideUp(options: { y?: string }, el: HTMLElement) {
   el.style.opacity = '0'
   el.style.transform = `translateY(${options.y ?? 60}px)`
 
   ScrollTrigger.create({
-      // markers: true,
-      trigger: el,
-      start: () => 'top+=200px bottom',
-      onToggle: self => {
-        // console.log('toggle, active: ', self.isActive, el)
-        if (!self.isActive) {
-          self.kill(false, true)
-          // console.log('killed')
-        } else {
-          // console.log('playing')
-          gsap.to(el, { opacity: 1, y: 0, ease: 'expo', duration: 1.5 })
-        }
-      },
-    })
+    // markers: true,
+    trigger: el,
+    start: () => 'top+=200px bottom',
+    onToggle: self => {
+      // console.log('toggle, active: ', self.isActive, el)
+      if (!self.isActive) {
+        self.kill(false, true)
+        // console.log('killed')
+      } else {
+        // console.log('playing')
+        gsap.to(el, { opacity: 1, y: 0, ease: 'expo', duration: 1.5 })
+      }
+    },
+  })
 }
 
-export function parallax(caller: string, img?: HTMLElement) {
+export function parallax(caller: string, img: HTMLElement) {
   if (img) {
     gsap.to(img, {
       ease: "none",
