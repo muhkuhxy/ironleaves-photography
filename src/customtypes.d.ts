8da4e86345195e1bdb19b179ba9b658299fe6dbd
Timon Loeffen <timon.loeffen@sigmatechnology.com>
2024-05-06 17:36:13 +0200

    clean pu

--- src/customtypes.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "bcrypt" {
-  function hashSync(plaintext: string, rounds: number): string
-
-  function compareSync(plaintext: string, hash: string): boolean
-}
-
-declare module "uuid" {
-  function v4(): string
-}
